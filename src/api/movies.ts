336ffc9bf0a9d86661392bec2867518779a914f2
roisnelgithub <147262667+roisnelgithub@users.noreply.github.com>
2024-04-25 07:27:29 -0400

    Update movies.ts

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,5 +1,5 @@
 export const getMovies = async (name: string) => {
-  const res = await fetch(`https://www.omdbapi.com/?apikey=2cdaa36f&s=${name}`);
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}=${name}`);
   const data = await res.json();
   return data;
 };
