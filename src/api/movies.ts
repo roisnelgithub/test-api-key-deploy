feaa41465cd8f0e4263645f495019f28c928f413
Roisnel <roy.adan.trenal@gmail.com>
2024-04-25 07:04:21 -0400

    fix: :test_tube: Test

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,7 +1,5 @@
 export const getMovies = async (name: string) => {
-  const res = await fetch(
-    `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${name}`
-  );
+  const res = await fetch(`https://www.omdbapi.com/?apikey=2cdaa36f&s=${name}`);
   const data = await res.json();
   return data;
 };
