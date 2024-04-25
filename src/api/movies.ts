a3b57048c5399c3cdd7f3aedd4273c91a33cd9b9
roisnelgithub <147262667+roisnelgithub@users.noreply.github.com>
2024-04-25 07:31:51 -0400

    Update movies.ts
    fix bug

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,5 +1,5 @@
 export const getMovies = async (name: string) => {
-  const res = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}=${name}`);
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${name}`);
   const data = await res.json();
   return data;
 };
